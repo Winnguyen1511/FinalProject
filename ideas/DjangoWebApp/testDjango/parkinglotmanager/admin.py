2bb803542dc7b033959cc869c719c3169530c8bd
khoa-CPC <khoanguyen1507dn@gmail.com>
2020-07-01 15:38:36 +0700

    update

---
+++ ideas/DjangoWebApp/testDjango/parkinglotmanager/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import History, Parking, Parkinglotlist, Stafflist,Cameralist
+
+
+admin.site.register(History)
+admin.site.register(Parking)
+admin.site.register(Parkinglotlist)
+admin.site.register(Stafflist)
+admin.site.register(Cameralist)
