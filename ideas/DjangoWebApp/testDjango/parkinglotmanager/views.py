741b65cc357b34fbf47c781e16d00d6707f5bfb2
Winnguyen <khoanguyen1507vn@gmail.com>
2020-10-01 21:38:07 +0700

    update role

--- ideas/DjangoWebApp/testDjango/parkinglotmanager/views.py
+++ ideas/DjangoWebApp/testDjango/parkinglotmanager/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, permission_required
 
 # Create your views here.
+
+@login_required
+@permission_required("parkinglotmanager.view_history")
+@permission_required("parkinglotmanager.view_parkinglotlist")
+@permission_required("parkinglotmanager.view_parking")
 def index(request):
     return render(request, 'parkinglotmanager/index.html')
 
+@login_required
 def edit(request):
     return render(request, 'parkinglotmanager/edit.html')
